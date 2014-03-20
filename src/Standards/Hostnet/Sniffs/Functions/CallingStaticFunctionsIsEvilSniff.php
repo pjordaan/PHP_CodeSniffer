b8227957c2d8a04da61b83a3468a23da43a51d32
Nico Schoenmaker <nschoenmaker@hostnet.nl>
2014-03-20 15:28:31 +0100

    Added a unit-tested static method calling check

---
+++ src/Standards/Hostnet/Sniffs/Functions/CallingStaticFunctionsIsEvilSniff.php
@@ -0,0 +1,16 @@
+<?php
+
+class Hostnet_Sniffs_Functions_CallingStaticFunctionsIsEvilSniff implements PHP_CodeSniffer_Sniff
+{
+  public function register()
+  {
+    // TODO: Auto-generated method stub
+    return array();
+  }
+
+  public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+  {
+    // TODO: Auto-generated method stub
+  }
+
+}
