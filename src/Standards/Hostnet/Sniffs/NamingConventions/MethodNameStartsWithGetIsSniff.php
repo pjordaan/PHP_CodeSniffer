01f08f5ec39bffcd1f48152cbd3ddef6b7c3993b
Iltar van der Berg <ivanderberg@hostnet.nl>
2014-03-20 20:54:29 +0100

    codestyle

--- src/Standards/Hostnet/Sniffs/NamingConventions/MethodNameStartsWithGetIsSniff.php
+++ src/Standards/Hostnet/Sniffs/NamingConventions/MethodNameStartsWithGetIsSniff.php
@@ -9,27 +9,27 @@ class Hostnet_Sniffs_NamingConventions_MethodNameStartsWithGetIsSniff implements
             T_FUNCTION
         );
     }
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    public function process(PHP_CodeSniffer_File $phpcs_file, $stack_ptr)
     {
         // search till T_STRING
         $index = 0;
-        while (isset($phpcsFile->getTokens()[$stackPtr + (++ $index)]) && $phpcsFile->getTokens()[$stackPtr + ($index)]['type'] !== 'T_STRING');
+        while (isset($phpcs_file->getTokens()[$stack_ptr + (++ $index)]) && $phpcs_file->getTokens()[$stack_ptr + ($index)]['type'] !== 'T_STRING');
 
-        $ptr = $stackPtr + $index;
+        $ptr = $stack_ptr + $index;
 
-        $f_name = $phpcsFile->getTokens()[$ptr]['content'];
+        $f_name = $phpcs_file->getTokens()[$ptr]['content'];
         if (preg_match('~^getIS~', $f_name)) {
             return;
         }
 
         if (preg_match('~^get([Ii]s[A-Z0-9]{1}.*)~', $f_name, $matches)) {
             $suggested = 'i' . substr($matches[0], 4);
-            $phpcsFile->addError('Invalid method name to get Boolean value. Suggested: ' . $suggested, $ptr);
+            $phpcs_file->addError('Invalid method name to get Boolean value. Suggested: ' . $suggested, $ptr);
             return;
         }
 
         if (preg_match('~^getis[a-zA-Z0-9]*~', $f_name)) {
-            $phpcsFile->addError('Invalid method name, do not use getis(.*)', $ptr);
+            $phpcs_file->addError('Invalid method name, do not use getis(.*)', $ptr);
             return;
         }
     }
