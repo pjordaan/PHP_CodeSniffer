de59d108e4405a03ad5c717d973c4a53dfa04e7d
Iltar van der Berg <ivanderberg@hostnet.nl>
2014-03-20 20:52:47 +0100

    added getIs something support

--- src/Standards/Hostnet/Sniffs/NamingConventions/MethodNameStartsWithGetIsSniff.php
+++ src/Standards/Hostnet/Sniffs/NamingConventions/MethodNameStartsWithGetIsSniff.php
@@ -2,15 +2,35 @@
 
 class Hostnet_Sniffs_NamingConventions_MethodNameStartsWithGetIsSniff implements PHP_CodeSniffer_Sniff
 {
-  public function register()
-  {
-    // TODO: Auto-generated method stub
-    return array();
-  }
+    public function register()
+    {
+        // TODO: Auto-generated method stub
+        return array(
+            T_FUNCTION
+        );
+    }
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // search till T_STRING
+        $index = 0;
+        while (isset($phpcsFile->getTokens()[$stackPtr + (++ $index)]) && $phpcsFile->getTokens()[$stackPtr + ($index)]['type'] !== 'T_STRING');
 
-  public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-  {
-    // TODO: Auto-generated method stub
-  }
+        $ptr = $stackPtr + $index;
 
+        $f_name = $phpcsFile->getTokens()[$ptr]['content'];
+        if (preg_match('~^getIS~', $f_name)) {
+            return;
+        }
+
+        if (preg_match('~^get([Ii]s[A-Z0-9]{1}.*)~', $f_name, $matches)) {
+            $suggested = 'i' . substr($matches[0], 4);
+            $phpcsFile->addError('Invalid method name to get Boolean value. Suggested: ' . $suggested, $ptr);
+            return;
+        }
+
+        if (preg_match('~^getis[a-zA-Z0-9]*~', $f_name)) {
+            $phpcsFile->addError('Invalid method name, do not use getis(.*)', $ptr);
+            return;
+        }
+    }
 }
--- test/Standards/Hostnet/NamingConventions/MethodNameStartsWithGetIsUnitTest.php
+++ test/Standards/Hostnet/NamingConventions/MethodNameStartsWithGetIsUnitTest.php
@@ -16,7 +16,7 @@ class Hostnet_NamingConventions_MethodNameStartsWithGetIsUnitTest extends Abstra
      */
     public function getErrorList($filename = null)
     {
-        return array(13 => 1, 17 => 1);
+        return array(13 => 1, 17 => 1, 35 => 1, 39 => 1, 43 => 1, 49 => 1, 55 => 1, 59 => 1, 73 => 1, 78 => 1, 83 => 1);
     }//end getErrorList()
 
     /**
